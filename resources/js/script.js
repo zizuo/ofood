6693b5847610ac5980b02b787c0ab17165450dc1
Aizatbek <abakalbaev@gmail.com>
2019-05-28 15:52:21 +0600

    Adding animations on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -78,4 +78,30 @@ $(document).ready(function () {
       });
 
 
+    // Animations on scroll
+    //https://daneden.github.io/animate.css/
+    $('.js--wp-1').waypoint(function () {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+       offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function () {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+       offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function () {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+       offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function () {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+       offset: '50%'
+    });
+
 });
