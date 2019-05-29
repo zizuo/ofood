58cc5919fdd53da520053b3a3dd37a8e7a5afc9a
Aizatbek <abakalbaev@gmail.com>
2019-05-29 17:22:32 +0600

    Making  the navigation menu responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -104,4 +104,20 @@ $(document).ready(function () {
        offset: '50%'
     });
 
+
+    // Mobile nav menu
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-ios-menu')) {
+            icon.addClass('ion-ios-close');
+            icon.removeClass('ion-ios-menu');
+        } else {
+            icon.addClass('ion-ios-menu');
+            icon.removeClass('thion-ios-close');
+        }
+    })
+
 });
