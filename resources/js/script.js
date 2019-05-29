c09a4ae2f1ddca01de6d754101d99c452fc35b1f
Aizatbek <abakalbaev@gmail.com>
2019-05-29 17:25:51 +0600

    refactor

--- resources/js/script.js
+++ resources/js/script.js
@@ -112,11 +112,11 @@ $(document).ready(function () {
 
         nav.slideToggle(200);
         if (icon.hasClass('ion-ios-menu')) {
-            icon.addClass('ion-ios-close');
+            icon.addClass('ion-md-close');
             icon.removeClass('ion-ios-menu');
         } else {
             icon.addClass('ion-ios-menu');
-            icon.removeClass('thion-ios-close');
+            icon.removeClass('ion-md-close');
         }
     })
 
